1c5ef71dfefe02527507751fdb1b9c77b7086535
Andriy-24 <andriymov4ko24@gmail.com>
2025-02-01 12:47:06 +0200

    Additional

--- HomeWork 3/Additional1/main.js
+++ HomeWork 3/Additional1/main.js
@@ -246,3 +246,125 @@ for (const user of usersList) {
         </div>
     </div>`)
 }
+
+// 5. Вивести кожен елемент масиву, сусід справа якого є парним
+// let array = [ 1, 2, 3, 5, 7, 9, 56, 8, 67 ]
+// for (let i = 0; i < array.length; i++) {
+//     if (array[i] % 2 === 0) {
+//         console.log(array[i-1]);
+//     }
+// }
+
+// 3. Вивести за допомогою console.log кожен третій елемен тільки якщо цей елемент є парним.
+
+// for (let i = 0; i < 50; i=i+3) {
+//     if (i % 2 === 0) {
+//         console.log(i)
+//     }
+// }
+
+// Непарне
+
+// for (let i = 0; i < 50; i=i+3) {
+//     if (i % 2 !== 0) {
+//         console.log(i)
+//     }
+// }
+
+// 6. Є масив з числами [100,250,50,168,120,345,188], Які характеризують вартість окремої покупки. Обрахувати середній чек.
+// let chekArray = [100,250,50,168,120,345,188]
+// let chek = 0;
+// for (let i = 0; i < chekArray.length; i++) {
+//     chek += chekArray[i];
+// }
+// sChek = chek / chekArray.length;
+// console.log(sChek);
+
+// 7. Створити масив з рандомними значеннями, помножити всі його елементи на 5 та перемістити їх в інший масив.
+// let chekArray = [100,250,50,168,120,345,188]
+// result = []
+// for (let i = 0; i < chekArray.length; i++) {
+//     result[i] = chekArray[i] * 5;
+// }
+// console.log(result)
+
+// - Дано 2 масиви з рівною кількістю об'єктів.
+// Масиви:
+//     let usersWithId = [
+//         {id: 1, name: 'vasya', age: 31, status: false},
+//         {id: 2, name: 'petya', age: 30, status: true},
+//         {id: 3, name: 'kolya', age: 29, status: true},
+//         {id: 4, name: 'olya', age: 28, status: false}
+//     ];
+
+// let citiesWithId = [
+//     {user_id: 3, country: 'USA', city: 'Portland'},
+//     {user_id: 1, country: 'Ukraine', city: 'Ternopil'},
+//     {user_id: 2, country: 'Poland', city: 'Krakow'},
+//     {user_id: 4, country: 'USA', city: 'Miami'}
+// ];
+
+// З'єднати в один об'єкт користувача та місто з відповідними "id" та "user_id" .
+//     Записати цей об'єкт в новий масив
+// Example:
+//     let usersWithCities = [
+//         {
+//             id: 1, // <===
+//             name: 'vasya',
+//             age: 31,
+//             status: false,
+//             address: {
+//                 user_id: 1, // <===
+//                 country: 'Ukraine',
+//                 city: 'Ternopil'
+//             }
+//         },
+//         // TO BE CONTINUED .....
+//     ]
+
+// let usersWithCities = [];
+// for (let i = 0; i < usersWithId.length; i++) {
+//     let user = usersWithId[i];
+//     let userAddress;
+//
+//     for (let j = 0; j < citiesWithId.length; j++) {
+//          if (citiesWithId[j].user_id === user.id) {
+//              userAddress = citiesWithId[j];
+//          }
+//     }
+//     usersWithCities[i] = {...user, address: userAddress};
+// }
+// console.log(usersWithCities);
+
+
+// - Взяти масив з 10 чисел або створити його. Створити 2й порожній масив. За допомогою будь-якого циклу скопіювати значення одного масиву в інший.
+
+// let nums = [100,250,50,168,120,345,188,255,12,464]
+// let numsCopy = []
+//
+// for (let i = 0; i < nums.length; i++) {
+//     numsCopy[i] = nums[i];
+// }
+// console.log(numsCopy);
+
+// - Дано масив: [ 'a', 'b', 'c'] . За допомогою циклу for зібрати всі букви в слово.
+// - Дано масив: [ 'a', 'b', 'c'] . За допомогою циклу while зібрати всі букви в слово.
+// - Дано масив: [ 'a', 'b', 'c'] . За допомогою циклу for of зібрати всі букви в слово.
+
+// let abc = [ 'a', 'b', 'c'];
+// let x = '';
+
+// for (let i = 0; i < abc.length; i++) {
+//     x += abc[i];
+// }
+
+// let i = 0;
+// while (i<abc.length) {
+//     x += abc[i];
+//     i++;
+// }
+
+// for (let string of abc) {
+//     x += string;
+// }
+// console.log(x)
